6d1026cb1c36f973805a3fa2c387d82523be5882
keoy7am <keoy7am@gmail.com>
2022-06-10 04:16:52 +0800

    feat: 設計師後台-完成登入Api串接

--- app/services/auth.server.ts
+++ app/services/auth.server.ts
@@ -2,6 +2,11 @@
 import { Authenticator, AuthorizationError } from 'remix-auth';
 import { FormStrategy } from 'remix-auth-form';
 import { sessionStorage, User } from '~/services/session.server';
+import { ApiClient } from 'app/ApiClient';
+import { OpenAPI } from 'app/ApiClient';
+import { LoginRequest } from 'app/ApiClient/models/LoginRequest';
+
+OpenAPI.BASE = 'https://serp.source-test.com:443'; // TODO 從設定檔讀取
 
 const authenticator = new Authenticator<User | Error | null>(sessionStorage, {
   sessionKey: "sessionKey", // keep in sync
@@ -14,8 +19,7 @@ authenticator.use(
     // 從表單獲取登入資訊
     let account = form.get('account') as string;
     let password = form.get('password') as string;
-    console.log("Account123: "+ account);
-    let user = null;
+    console.log("Account123: " + account);
 
     // 輸入驗證
     if (!account || account?.length === 0) throw new AuthorizationError('帳號為必填欄位')
@@ -27,28 +31,38 @@ authenticator.use(
       throw new AuthorizationError('密碼必須為字串型別')
 
     // 模擬登入
-    if (account === 'test' && password === '123456') {
-      user = {
-        name: account,
-        token: `${password}-${new Date().getTime()}`,
-      };
+    // if (account === 'test' && password === '123456') {
+    //   user = {
+    //     name: account,
+    //     token: `${password}-${new Date().getTime()}`,
+    //   };
 
-      // the type of this user must match the type you pass to the Authenticator
-      // the strategy will automatically inherit the type if you instantiate
-      // directly inside the `use` method
-      return await Promise.resolve({ ...user });
+    //   // the type of this user must match the type you pass to the Authenticator
+    //   // the strategy will automatically inherit the type if you instantiate
+    //   // directly inside the `use` method
+    //   return await Promise.resolve({ ...user });
 
-    } else {
-      // if problem with user throw error AuthorizationError
-      throw new AuthorizationError("輸入錯誤")
-    }
+    // } else {
+    //   // if problem with user throw error AuthorizationError
+    //   throw new AuthorizationError("輸入錯誤")
+    // }
+    let apiClient = new ApiClient();
+    let loginRequest : LoginRequest = {
+      password: password,
+      username: account
+    };
 
-    // TODO: 透過API登入
-    // if login
-    //    something
-    // else
-    //    something
+    let user = await apiClient.accountController.loginUsingPost(loginRequest)
+    .then(res => {
+      return res;
+    });
 
+    if(user === undefined)
+      throw new AuthorizationError("登入異常");
+    if(user.success === false)
+      throw new AuthorizationError(user.message);
+
+    return Promise.resolve(user);
   }),
   "user-login"
 );
