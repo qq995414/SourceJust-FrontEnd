c50998676590f2f6d4e4d07affd6ff14db9be9de
qq995414 <80391152+qq995414@users.noreply.github.com>
2022-08-15 19:17:06 +0800

    feat: admin新版修改

--- app/components/SideNav.tsx
+++ app/components/SideNav.tsx
@@ -1,8 +1,15 @@
 import { Link, useLocation } from 'remix';
+import { useLoaderData } from 'remix';
+import { useState } from 'react';
 
 interface SideNavLink {
   activeIconUrl: string;
   inactiveIconUrl: string;
+  activeIconColor: string;
+  inactiveIconColor: string;
+  activeBgColor: string,
+  inactiveBgColor: string,
+  targettext: string;
   link: string;
   match: RegExp;
 }
@@ -13,50 +20,85 @@ interface Props {
 }
 
 export default function (props: Props) {
-
+  const data = useLoaderData();
+  const [sidenavshow, setsidenavshow] = useState(true);
+  console.log(data);
   const location = useLocation();
+  return (
+    <div className="fixed top-0 left-0 h-screen bg-white shadow-xl 
+     rounded-lg flex flex-col  ">
+       {sidenavshow === true &&
+             <img className="mb-5 " src={'/icons/sidenav-logo.svg'} alt="" />
+       }
+       {sidenavshow === false &&
+             <img className="mb-5 w-full" src={'/icons/logo.svg'} alt="" />
+       }
+      {sidenavshow === true &&
+        <div className="flex flex-col align-center w-full pt-4 ">
 
-  return <div
-    className="fixed top-0 left-0 h-screen bg-white shadow-xl
-  p-4 rounded-lg flex flex-col justify-between">
-    <div className="flex flex-col align-center w-full">
-      <Link to="/">
-        <img src={'/images/logo.svg'} className="w-12 h-12 mx-auto" alt="" />
-      </Link>
-      {props.links.map((link, i) => {
-        const isActive = location.pathname.match(link.match);
-        return <div key={i} className="mx-auto mt-12">
-          <Link
-            to={link.link}
-            prefetch="intent"
-          >
-            <img
-              src={isActive ? link.activeIconUrl : link.inactiveIconUrl}
-              alt="" />
-          </Link>
-        </div>;
-      })}
-    </div>
-    <div className="w-full flex flex-col justify-center align-center">
-      {!location.pathname.includes('admin') &&
-        <Link
-          prefetch="intent"
-          to={'/' + location.pathname.split('/')[1] + '/notifications'}>
-          <img
-            className="mx-auto mb-12"
-            src={location.pathname.includes('notifications')
-              ? '/icons/notifications.svg' : '/icons/notifications-gray.svg'}
-            alt="" />
-        </Link>
+          {props.links.map((link, i) => {
+            const isActive = location.pathname.match(link.match);
+            
+            return <div className=' w-full pt-1 pb-1'  >
+              <div key={i} className={isActive? `w-full isActive-off  mx-auto  pt-4 pb-4 px-4 ` :  ` w-full isActive-on  mx-auto  pt-4 pb-4 px-4`} >
+                <Link 
+                  to={link.link}
+                  prefetch="intent"
+                >
+                  <img
+                    src={isActive ? link.activeIconUrl : link.inactiveIconUrl}
+                    className=' w-4'
+                    alt="" />
+                </Link>
+              </div>
+            </div>;
+          })}
+        </div>
       }
-      <Link
-        prefetch="intent"
-        to={'/' + location.pathname.split('/')[1] + '/account'}>
-        <img
-          className="w-14 h-14 rounded-full cursor-pointer mx-auto mb-12"
-          src={props.avatarUrl}
-          alt="" />
-      </Link>
+      {sidenavshow === false &&
+        <div className="flex flex-col align-center w-full pt-4 ">
+          {props.links.map((link, i) => {
+            const isActive = location.pathname.match(link.match);
+            const color = isActive ? link.activeIconColor : link.inactiveIconColor;
+            const bkcolor = isActive ? link.activeBgColor : link.inactiveBgColor
+            
+            return <div  className='w-full  pt-1 pb-1 '   >
+
+              <div key={i} className={isActive? `w-full isActive-off  mx-auto w-full  pt-3 pb-3 ` :  ` w-full isActive-on mx-auto w-full  pt-3 pb-3`}  >
+                <Link
+                  to={link.link}
+                  prefetch="intent"
+                >
+                  <div className='px-4'>
+
+                    <img
+                      src={isActive ? link.activeIconUrl : link.inactiveIconUrl}
+                      className=' w-4'
+                      style={{ display: "inline-block" }} /> <span className='pl-2 ' style={{ color: color }}>{link.targettext}</span>
+                  </div>
+
+                </Link>
+              </div>
+            </div>
+              ;
+          })}
+        </div>
+      }
+      {sidenavshow === true &&
+        <div className="w-full flex mt-96 flex-col justify-center align-center">
+          <div className='w-12' style={{ boxShadow: 'inset 0px 1px 0px #F0F0F0' }}>
+            <a onClick={() => setsidenavshow(false)} >YES</a>
+          </div>
+        </div>
+      }
+      {sidenavshow === false &&
+        <div className="w-full flex mt-96 flex-col justify-center align-center">
+          <div className='w-36' style={{ boxShadow: 'inset 0px 1px 0px #F0F0F0' }}>
+            <a onClick={() => setsidenavshow(true)} >NO</a>
+          </div>
+        </div>
+      }
     </div>
-  </div>;
+
+  );
 }
--- app/routes/admin.tsx
+++ app/routes/admin.tsx
@@ -7,27 +7,65 @@ export const meta: MetaFunction = () => {
 
 export default function Index() {
   return (
-    <>
+    <>  
       <SideNav
         avatarUrl="https://media.istockphoto.com/photos/smiling-female-architect-sitting-at-her-office-desk-picture-id1287459398?k=20&m=1287459398&s=612x612&w=0&h=-BH-AsvbdjM8G-Uc_UabWztbQxFmYstg_Z2-Ix4_IEg="
         links={[
           {
             activeIconUrl: '/icons/overview.svg',
             inactiveIconUrl: '/icons/overview-gray.svg',
+            activeIconColor: '#B62122',
+            inactiveIconColor: '#BBBBBB',
+            activeBgColor: '#FFF1F3',
+            inactiveBgColor: '#FFFFFF',
             link: '/admin/dashboard',
+            targettext: "總覽",
             match: /^\/admin\/dashboard/,
           },
           {
-            activeIconUrl: '/icons/projects.svg',
-            inactiveIconUrl: '/icons/projects-gray.svg',
-            link: '/admin/projects',
-            match: /^\/admin\/projects/,
+            activeIconUrl: '/icons/blog.svg',
+            inactiveIconUrl: '/icons/blog-gray.svg',
+            activeIconColor: '#B62122',
+            inactiveIconColor: '#BBBBBB',
+            activeBgColor: '#FFF1F3',
+            inactiveBgColor: '#FFFFFF',
+            link: '/admin/webpage',
+            targettext: "部落格管理",
+            match: /^\/admin\/webpage/,
+          },
+
+          {
+            activeIconUrl: '/icons/clientexam.svg',
+            inactiveIconUrl: '/icons/clientexam-gray.svg',
+            activeIconColor: '#B62122',
+            inactiveIconColor: '#BBBBBB',
+            activeBgColor: '#FFF1F3',
+            inactiveBgColor: '#FFFFFF',
+            link: '/admin/clientexam',
+            targettext: "作品集管理",
+            match: /^\/admin\/clientexam/,
           },
           {
-            activeIconUrl: '/icons/users.svg',
-            inactiveIconUrl: '/icons/users-gray.svg',
+            activeIconUrl: '/icons/mail.svg',
+            inactiveIconUrl: '/icons/mail-gray.svg',
+            activeIconColor: '#B62122',
+            inactiveIconColor: '#BBBBBB',
+            activeBgColor: '#FFF1F3',
+            inactiveBgColor: '#FFFFFF',
             link: '/admin/users/chooseUsers',
+            targettext: "信件管理",
             match: /^\/admin\/users/,
+          },
+          {
+            activeIconUrl: '/icons/navi.svg',
+            inactiveIconUrl: '/icons/navi-gray.svg',
+            activeIconColor: '#B62122',
+            inactiveIconColor: '#BBBBBB',
+            activeBgColor: '#FFF1F3',
+            inactiveBgColor: '#FFFFFF',
+            link: '/admin/users/templateUsers',
+            targettext: "模板管理",
+            match: /^\/admin\/template/,
           }
         ]} />
       <Outlet />
