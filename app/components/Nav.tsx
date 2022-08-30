53d1c7ff27e07a8d2dac84029f7e84fe8db3ad66
qq995414 <80391152+qq995414@users.noreply.github.com>
2022-08-31 00:33:22 +0800

    fix:Table

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,7 +1,7 @@
 export function Nav() {
-    return <div className="w-full mx-auto">
-        <div className="bg-white flex flex-row w-full h-16 px-8 fixed top-0 pt-5 nav-title"
-            style={{ boxShadow: 'inset 0px -1px 0px #F0F0F0;' }}>
+    return <div className="w-full mx-auto" style={{ zIndex:"50"}}>
+        <div className=" flex flex-row w-full h-16 px-8 fixed top-0 pt-5 nav-title"
+            style={{ boxShadow: 'inset 0px -1px 0px #F0F0F0;',backgroundColor:'#FFFFFF' }}>
             總覽
         </div>
     </div>;
