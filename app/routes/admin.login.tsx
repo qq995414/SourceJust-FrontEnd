70aa4c1826430a96fdb4799faf1d571cb024c207
qq995414 <80391152+qq995414@users.noreply.github.com>
2022-09-06 00:10:13 +0800

    feat:管理員登入介面 and api串接

--- app/routes/admin.login.tsx
+++ app/routes/admin.login.tsx
@@ -54,7 +54,7 @@ interface Props {
 export default function Index() {
   const navigate = useNavigate();
   const loaderData = useLoaderData();
-  
+
   OpenAPI.TOKEN = loaderData?.data?.token;
   const [passwordShown, setPasswordShown] = useState(true);
 
@@ -62,31 +62,17 @@ export default function Index() {
   // const [Password, setPassword] = useState('');
   return (
     <div
-      className=" justify-center h-screen items-center overflow-hidden">
-      <div className="flex flex-row w-full h-16 px-8   pt-5"
+      className=" justify-center h-screen items-center overflow-hidden w-screen">
+      <div className="flex flex-row w-full h-16 px-8   pt-5 login-background "
         style={{ boxShadow: 'inset 0px -1px 0px #F0F0F0;' }}>
         <img className="mb-5" src={'/icons/logo.svg'} alt="" />
 
       </div>
-      <div className="pt-24 flex-row w-full h-full login-background">
-        <div className="w-screen p-4 flex flex-col justify-center items-center">
-          <img className="mb-2 w-48" src={'/icons/logo.svg'} alt="" />
-          <p style={{ color: 'rgba(0, 0, 0, 0.45)' }} className="text-center tracking-widest text-sm m-2">索爾斯專案後台管理系統</p>
-          <div
-            className=' pt-5 w-96'
-          >
-            <hr
-              style={{
-                color: 'red',
-                width: '100%',
-                border: '1px solid #E9E9E9  ',
-                borderRadius: 'radius: 2px'
-              }}></hr>
-          </div>
-
+      <div className="pt-36 flex flex-row w-full h-full  justify-center align-center">
+        <div className="p-4 flex flex-col justify-center h-128 items-center login-box">
           <p
             style={{ fontWeight: 800 }}
-            className="text-center tracking-widest text-2xl mt-10">登入</p>
+            className="text-center tracking-widest text-2xl ">登入</p>
           <div
             className=' pt-5 w-96'
           >
@@ -94,7 +80,7 @@ export default function Index() {
               <p className='pt-1 Neutral-Colors-DarkGrey text-sm' style={{ fontWeight: 800 }}>帳號</p>
               <p className='text-xs' style={{ color: '#7c7c7c' }}>請出入 ID 或 Email</p>
               <TextInput
-                className="mt-2 mx-auto w-full"
+                className="mt-2 mx-auto w-full neutral-colors-5-LightGrey-border"
                 type="sign"
                 placeholder="例如：Source9121"
                 itemName='account'
@@ -110,20 +96,21 @@ export default function Index() {
               <div className="pass-wrapper w-screen">
                 <TextInput
 
-                  className="mt-3 mx-auto w-screen"
+                  className="mt-3 mx-auto w-full neutral-colors-5-LightGrey-border"
                   type="sign"
                   placeholder="請輸入密碼"
                   itemName='password'
                   show={passwordShown}
                 />
-                 {passwordShown === true && <i className='mr-10' onClick={() => setPasswordShown(false)}><img className="mb-5" src={'/icons/eye-icon.svg'} alt=""  /></i>}
-                {passwordShown === false && <i className='mr-10' onClick={() => setPasswordShown(true)}><img className="mb-5" src={'/icons/eye-icon.svg'} alt=""  /></i>}
+                {passwordShown === true && <i className='mr-10' onClick={() => setPasswordShown(false)}><img className="mb-5" src={'/icons/eye-icon.svg'} alt="" /></i>}
+                {passwordShown === false && <i className='mr-10' onClick={() => setPasswordShown(true)}><img className="mb-5" src={'/icons/eye-icon.svg'} alt="" /></i>}
               </div>
               <div className="text-right mt-4 mr-1">
-
-
               </div>
-              <div className="mt-5 text-center">
+              <div className='flex flex-col'>
+                {loaderData?.error ? <p className='login-error flex'><img className="mb-5" src={'/icons/login-error.svg'} alt="" />  {loaderData?.error?.message}</p> : ""}
+              </div>
+              <div className="mt-1 text-center">
                 <Button
                   type="login"
                   onClick={() => console.log("點擊登入")}
@@ -131,9 +118,7 @@ export default function Index() {
                   登入
                 </Button>
               </div>
-              <div>
-                {loaderData?.error ? <p>錯誤訊息: {loaderData?.error?.message}</p> : null}
-              </div>
+
             </Form>
           </div>
         </div>
